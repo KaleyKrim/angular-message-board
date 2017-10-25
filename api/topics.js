0b17194ea145bc0b8a1fb57e96764f53c0ce5140
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-10-24 15:37:38 -1000

    deletes old format for routes

--- api/topics.js
+++
@@ -1,9 +0,0 @@
-//jshint esversion: 6
-
-const express = require('express');
-const router = express.Router();
-const db = require('../models');
-const Topic = db.topic;
-
-
-module.exports = router;
--- api/users.js
+++
@@ -1,9 +0,0 @@
-//jshint esversion: 6
-
-const express = require('express');
-const router = express.Router();
-const db = require('../models');
-const User = db.user;
-
-
-module.exports = router;
