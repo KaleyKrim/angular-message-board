65eab8360e685e69d105954d087b369d5668df30
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-10-26 16:57:06 -1000

    displays author name on latest list

--- public/js/controllers/latest.controller.js
+++ public/js/controllers/latest.controller.js
@@ -1,11 +1,16 @@
 angular.module('App')
-.controller('LatestController', ['MessageService', 'TopicService', '$scope', function(MessageService, TopicService, $scope) {
+.controller('LatestController', ['MessageService', 'TopicService', 'UserService', '$scope', function(MessageService, TopicService, UserService, $scope) {
 
   $scope.MessageService = MessageService;
   $scope.TopicService = TopicService;
+  $scope.UserService = UserService;
 
   MessageService.getLatest();
 
   $scope.getTopicNameById = TopicService.getTopicNameById;
+
+  UserService.getUsers();
+  $scope.findUserById = UserService.findUserById;
+
 
 }]);
