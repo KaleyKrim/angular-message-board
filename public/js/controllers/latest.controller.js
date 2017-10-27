77b9d6d17320c8d96883bb8b114a52c17aec3814
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-10-26 16:28:54 -1000

    topics on latest page display as topic names

--- public/js/controllers/latest.controller.js
+++ public/js/controllers/latest.controller.js
@@ -1,8 +1,11 @@
 angular.module('App')
-.controller('LatestController', ['MessageService', '$scope', function(MessageService, $scope) {
+.controller('LatestController', ['MessageService', 'TopicService', '$scope', function(MessageService, TopicService, $scope) {
 
   $scope.MessageService = MessageService;
+  $scope.TopicService = TopicService;
 
   MessageService.getLatest();
+
+  $scope.getTopicNameById = TopicService.getTopicNameById;
 
 }]);
